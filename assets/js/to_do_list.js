66aedce1b13ed49a04bba2f1c02cff4de351c5ae
Nestor Tamares <jrtamares@gmail.com>
2018-11-06 17:11:54 -0600

    Styling
    Actually looks good

--- assets/js/to_do_list.js
+++ assets/js/to_do_list.js
@@ -16,6 +16,11 @@ $("ul").on("click", "span", function(e){
     if(event.which === 13) {
         var newToDo = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span> X </span> " + newToDo + "</li>");
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i> </span> " + newToDo + "</li>");
     }
+ });
+
+ // toggle input
+ $(".fa-plus").click(function(){
+    $("input[type = 'text']").fadeToggle();
  });
